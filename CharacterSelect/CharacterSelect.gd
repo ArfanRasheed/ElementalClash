e292ba2a8bb08a83f921cc2e9ed54f4517a2543a
ArfanRasheed987 <144159965+ArfanRasheed987@users.noreply.github.com>
2023-11-19 15:43:01 -0500

    CharSelect updates

--- CharacterSelect/CharacterSelect.gd
+++ CharacterSelect/CharacterSelect.gd
@@ -1,38 +1,39 @@
 extends Node2D
 
+var PlayerSelect = 0 
 
 func _process(delta):
-	match EcGame.PlayerSelect:
+	match PlayerSelect:
 		0:
 			get_node("PlayerSelect").play("Player0")
-			get_node("PlayerSelect/Descrpt").text = "Name : Fire Knight \nAbilities : "
+			get_node("PlayerSelect/Descript").text = "Name : Fire Knight \nAbilities : "
 		1:
 			get_node("PlayerSelect").play("Player1")
-			get_node("PlayerSelect/Descrpt").text = "Name : Leaf Ranger \nAbilities : "
+			get_node("PlayerSelect/Descript").text = "Name : Leaf Ranger \nAbilities : "
 		2:
 			get_node("PlayerSelect").play("Player2")
-			get_node("PlayerSelect/Descrpt").text = "Name : Metal Bladeskeeper \nAbilities : "
+			get_node("PlayerSelect/Descript").text = "Name : Metal Bladeskeeper \nAbilities : "
 		3:
 			get_node("PlayerSelect").play("Player3")
-			get_node("PlayerSelect/Descrpt").text = "Name : Air Monk \nAbilities : "
+			get_node("PlayerSelect/Descript").text = "Name : Air Monk \nAbilities : "
 		4:
 			get_node("PlayerSelect").play("Player4")
-			get_node("PlayerSelect/Descrpt").text = "Name : Water Priestess \nAbilities : "
+			get_node("PlayerSelect/Descript").text = "Name : Water Priestess \nAbilities : "
 		5:
 			get_node("PlayerSelect").play("Player5")
-			get_node("PlayerSelect/Descrpt").text = "Name : Wind Hashimara \nAbilities : "
+			get_node("PlayerSelect/Descript").text = "Name : Wind Hashimara \nAbilities : "
 		
 func _on_left_pressed():
-	if EcGame.PlayerSelect >= 1:
-		EcGame.PlayerSelect -= 1
-	elif EcGame.PlayerSelect == 0:
-		EcGame.PlayerSelect = 6
+	if PlayerSelect >= 1:
+		PlayerSelect -= 1
+	elif PlayerSelect == 0:
+		PlayerSelect = 6
 
 func _on_right_pressed():
-	if EcGame.PlayerSelect < 5:
-		EcGame.PlayerSelect += 1
-	elif EcGame.PlayerSelect == 5:
-		EcGame.PlayerSelect = 0
+	if PlayerSelect < 5:
+		PlayerSelect += 1
+	elif PlayerSelect == 5:
+		PlayerSelect = 0
 
 
 func _on_ready_pressed():
