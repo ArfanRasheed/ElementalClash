02ca22e09efedfdd0feb52e67aaa04f88f39cae5
slucyber <144160135+slucyber@users.noreply.github.com>
2023-11-28 15:58:42 -0500

    Add files via upload

---
+++ MainMenu/MainMenu.gd
@@ -0,0 +1,32 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#Globals.volume2 = $HSlider2.value
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://CharacterSelect/CharacterSelect.tscn")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
+
+
+func _on_link_button_pressed():
+	OS.shell_open("https://forms.gle/3o5KY1WCarutDeRf6")
+
+
+func _on_settings_button_pressed():
+	get_tree().change_scene_to_file("res://Settings/Settings.tscn")
+
+
+func _on_discord_pressed():
+	OS.shell_open("https://discord.gg/DFmnsfPZCx")
